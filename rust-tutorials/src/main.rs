577600cbbe4983cfdb068ea2200ac9e645715eb7
prothy <lukacsurgay@gmail.com>
2023-03-12 19:15:14 +0100

    initialize sld window

---
+++ rust-tutorials/src/main.rs
@@ -0,0 +1,34 @@
+use sdl2::{event::Event, pixels::Color, render::Canvas, sys::Window};
+
+extern crate sdl2;
+
+// from docs https://docs.rs/sdl2/latest/sdl2/#
+fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("demo", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        canvas.clear();
+        canvas.present();
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. } => break 'running,
+                _ => {}
+            }
+        }
+    }
+}
