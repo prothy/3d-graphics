a11b11b6e064f9a497a135c6138d94923895c11e
Luka Csurgay <luka.csurgay@accedo.tv>
2023-03-30 15:22:15 +0200

    add shader compilation error handling

--- rust-tutorials/src/main.rs
+++ rust-tutorials/src/main.rs
@@ -1,4 +1,8 @@
 use sdl2::{event::Event, video::GLProfile};
+use std::{
+    error::Error,
+    ffi::{CStr, CString},
+};
 
 extern crate gl;
 extern crate sdl2;
@@ -10,7 +14,7 @@ fn main() {
 
     let gl_attr = video_subsystem.gl_attr();
     gl_attr.set_context_profile(GLProfile::Core);
-    gl_attr.set_context_version(4, 5);
+    gl_attr.set_context_version(4, 1);
 
     let window = video_subsystem
         .window("demo", 800, 600)
@@ -42,4 +46,44 @@ fn main() {
 
         window.gl_swap_window();
     }
+}
+
+// http://nercury.github.io/rust/opengl/tutorial/2018/02/10/opengl-in-rust-from-scratch-03-compiling-shaders.html
+fn shader_from_source(source: &CStr, kind: gl::types::GLuint) -> Result<gl::types::GLuint, String> {
+    let id = unsafe { gl::CreateShader(kind) };
+
+    unsafe {
+        gl::ShaderSource(id, 1, &source.as_ptr(), std::ptr::null());
+        gl::CompileShader(id)
+    }
+
+    let mut success: gl::types::GLint = 1;
+
+    unsafe { gl::GetShaderiv(id, gl::COMPILE_STATUS, &mut success) }
+
+    if success == 0 {
+        let mut len: gl::types::GLint = 0;
+
+        unsafe {
+            gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &mut len);
+        }
+
+        let mut buffer: Vec<u8> = Vec::with_capacity(len as usize + 1);
+        buffer.extend([b' '].iter().cycle().take(len as usize));
+
+        let error: CString = unsafe { CString::from_vec_unchecked(buffer) };
+
+        unsafe {
+            gl::GetShaderInfoLog(
+                id,
+                len,
+                std::ptr::null_mut(),
+                error.as_ptr() as *mut gl::types::GLchar,
+            );
+
+            return Err(error.to_string_lossy().into_owned());
+        }
+    }
+
+    Ok(id)
 }
