90334405689ebea9d31c35967a4642bc7c2b2b17
Luka Csurgay <luka.csurgay@accedo.tv>
2023-03-31 11:10:57 +0200

    add error handling to program struct

--- rust-tutorials/src/render_gl.rs
+++ rust-tutorials/src/render_gl.rs
@@ -2,22 +2,83 @@ pub mod render_gl {
     use gl;
     use std;
     use std::ffi::{CStr, CString};
-    struct Shader {
+
+    pub struct Program {
+        id: gl::types::GLuint,
+    }
+
+    impl Program {
+        pub fn from_shaders(shaders: &[Shader]) -> Result<Program, String> {
+            let program_id = unsafe { gl::CreateProgram() };
+
+            for shader in shaders {
+                unsafe {
+                    gl::AttachShader(program_id, shader.id());
+                }
+            }
+
+            let mut success: gl::types::GLint = 1;
+
+            unsafe {
+                gl::LinkProgram(program_id);
+                gl::GetShaderiv(program_id, gl::LINK_STATUS, &mut success);
+            }
+
+            if success == 0 {
+                let mut len: gl::types::GLint = 0;
+
+                unsafe {
+                    gl::GetShaderiv(program_id, gl::INFO_LOG_LENGTH, &mut len);
+                }
+
+                let error = create_whitespace_cstring_with_len(len as usize);
+
+                unsafe {
+                    gl::GetShaderInfoLog(
+                        program_id,
+                        len,
+                        std::ptr::null_mut(),
+                        error.as_ptr() as *mut gl::types::GLchar,
+                    );
+
+                    return Err(error.to_string_lossy().into_owned());
+                }
+            }
+
+            for shader in shaders {
+                unsafe {
+                    gl::DetachShader(program_id, shader.id());
+                }
+            }
+
+            Ok(Program { id: program_id })
+        }
+
+        pub fn use_program(&self) {
+            unsafe { gl::UseProgram(self.id) }
+        }
+    }
+
+    pub struct Shader {
         id: gl::types::GLuint,
     }
 
     impl Shader {
-        fn from_source(source: &CStr, kind: gl::types::GLuint) -> Result<Shader, String> {
+        pub fn from_source(source: &CStr, kind: gl::types::GLuint) -> Result<Shader, String> {
             let id = shader_from_source(source, kind)?;
             Ok(Shader { id })
         }
 
-        fn from_vert_source(source: &CStr) -> Result<Shader, String> {
+        pub fn from_vert_source(source: &CStr) -> Result<Shader, String> {
             Shader::from_source(source, gl::VERTEX_SHADER)
         }
 
-        fn from_frag_source(source: &CStr) -> Result<Shader, String> {
+        pub fn from_frag_source(source: &CStr) -> Result<Shader, String> {
             Shader::from_source(source, gl::FRAGMENT_SHADER)
+        }
+
+        pub fn id(&self) -> gl::types::GLuint {
+            self.id
         }
     }
 
